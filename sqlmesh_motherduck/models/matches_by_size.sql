c3cec4952bd5e6771ff132bcbce7d6eb6b81ab17
Pavlo Kurochka <kurochka@gmail.com>
2024-04-17 18:38:16 -0700

    matches_by_size

---
+++ sqlmesh_motherduck/models/matches_by_size.sql
@@ -0,0 +1,22 @@
+MODEL (
+  name facts.matches_by_size,
+  kind FULL
+);
+
+WITH by_date AS (
+SELECT
+	  match_size, 
+	 startgame_date,
+		count(1) count_
+FROM
+		coh3.facts.matches mm
+WHERE
+len(ai_difficulty) =0
+	AND description = 'AUTOMATCH'
+GROUP BY
+	ALL)
+	PIVOT by_date ON
+match_size
+	USING sum(count_)
+ORDER BY
+startgame_date
