e0e1256bef6f543dadfd9f23cd83b2c3866d93b4
Pavlo Kurochka <36898697+pavlokurochka@users.noreply.github.com>
2024-04-18 03:19:22 +0000

    cleaned dashboard

--- dashboard.py
+++ dashboard.py
@@ -27,25 +27,13 @@ df = con.sql("""WITH by_date AS (
 				ORDER BY
 				startgame_date""").df().set_index("startgame_date")
 # %%
+st.set_page_config(
+	page_title="COH3 Data",
+	page_icon="🪖",
+)
 st.markdown("_Count of Matches by Size_")
 st.dataframe(df)
 
-# %%
-# df = con.sql("""SELECT mp.name map_name,
-# 		count(1) count_
-# 	FROM
-# 		coh3.fact.matches mm
-#   join coh3.dim.match_types mt on mm.matchtype_id = mt.id
-#   left join coh3.dim.maps mp on mm.mapname = mp.id
-#   where mt."name" not like '%Ai%'
-#              and left(mt.name,3)  = '1V1'
-#              and epoch_ms(startgametime*1000)::date = '2024-04-08'
-#   and      mm.description = 'AUTOMATCH'
-# 		GROUP BY ALL 
-# 		ORDER BY 1,2 
-#         limit 10""").df()
-# # %%
-# st.markdown("_1v1 Maps_")
 st.line_chart(df)
 # %%
 # set MOTHERDUCK_TOKEN=<tocken>
