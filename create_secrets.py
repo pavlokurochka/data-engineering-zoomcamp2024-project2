5b1de1dbc6ebb54ea731254c73aa92b23e9cd5fe
Pavlo Kurochka <36898697+pavlokurochka@users.noreply.github.com>
2024-06-04 22:06:35 +0000

    github actions 1

---
+++ create_secrets.py
@@ -0,0 +1,13 @@
+#%%
+import os
+#%%
+contents = f"""[destination.motherduck.credentials]
+database = "coh3"  
+password = "{os.environ['MOTHERDUCK_TOKEN'].strip()}" """ 
+with open(os.path.join('dlt','.dlt','secrets.toml'), 'w', encoding='utf-8') as f:
+    f.write(contents)
+#%%
+with open(os.path.join('sqlmesh_motherduck','config_template.yaml'), "r", encoding="utf-8") as file:
+    contents = file.read().replace('<motherduck_token>', os.environ['MOTHERDUCK_TOKEN'].strip())
+with open(os.path.join('sqlmesh_motherduck','config.yaml'), 'w', encoding='utf-8') as file:
+    file.write(contents)    
