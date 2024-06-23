95c67e6f91e7d318a93088be7ae5b629bb890481
Pavlo Kurochka <kurochka@gmail.com>
2024-06-23 16:21:01 -0700

    versions test

---
+++ versions_test.py
@@ -0,0 +1,11 @@
+#%%
+import pandas
+import streamlit
+import duckdb
+import dlt
+# %%
+print(f"{pandas.__version__=}")
+print(f"{streamlit.__version__=}")
+print(f"{duckdb.__version__=}")
+print(f"{dlt.__version__=}")
+# %%
