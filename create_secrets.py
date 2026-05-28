10e17ee9faccfab6975a8408b9bca3d3bd58defe
Pavlo Kurochka <kurochka@gmail.com>
2026-05-28 13:56:19 -0700

    run in win

--- create_secrets.py
+++ create_secrets.py
@@ -1,14 +1,29 @@
-"""Create files with secretsfor github actions .github\workflows\data-pipeline.yml """
-#%%
+"""Create files with secretsfor github actions .github\workflows\data-pipeline.yml"""
+
+# %%
 import os
-#%%
+
+# %%
+# from dotenv import load_dotenv
+
+# # Load variables from .env file
+# load_dotenv(".env")
+# %%
 contents = f"""[destination.motherduck.credentials]
 database = "coh3"  
-password = "{os.environ['MOTHERDUCK_TOKEN'].strip()}" """ 
-with open(os.path.join('dlt_motherduck','.dlt','secrets.toml'), 'w', encoding='utf-8') as f:
+password = "{os.environ['MOTHERDUCK_TOKEN'].strip()}" """
+with open(
+    os.path.join("dlt_motherduck", ".dlt", "secrets.toml"), "w", encoding="utf-8"
+) as f:
     f.write(contents)
-#%%
-with open(os.path.join('sqlmesh_motherduck','config_template.yaml'), "r", encoding="utf-8") as file:
-    contents = file.read().replace('<motherduck_token>', os.environ['MOTHERDUCK_TOKEN'].strip())
-with open(os.path.join('sqlmesh_motherduck','config.yaml'), 'w', encoding='utf-8') as file:
-    file.write(contents)    
+# %%
+with open(
+    os.path.join("sqlmesh_motherduck", "config_template.yaml"), "r", encoding="utf-8"
+) as file:
+    contents = file.read().replace(
+        "<motherduck_token>", os.environ["MOTHERDUCK_TOKEN"].strip()
+    )
+with open(
+    os.path.join("sqlmesh_motherduck", "config.yaml"), "w", encoding="utf-8"
+) as file:
+    file.write(contents)
